09bc07f342570c3449c6dc4510de9474cb566237
imangali <i.kaliakhmetov@strongte.am>
2023-09-03 12:42:48 +0600

    Initial Commit

---
+++ src/main/java/com/zefir/ZefirApplication.java
@@ -0,0 +1,11 @@
+package com.zefir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZefirApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZefirApplication.class, args);
+    }
+}
