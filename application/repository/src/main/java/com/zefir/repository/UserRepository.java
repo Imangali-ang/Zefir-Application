b408c2778f6fcc470ab021380acb5be354a50e0c
imangali <i.kaliakhmetov@strongte.am>
2023-09-17 15:50:02 +0600

    user repository

--- application/repository/src/main/java/com/zefir/repository/UserRepository.java
+++ application/repository/src/main/java/com/zefir/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package com.zefir.repository;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.zefir.models.User;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 
+    @Query("select u from User u where u.email=:email")
+    Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("select u from User u where u.id = :id")
+    Optional<User> findById(@Param("id")UUID id);
 }
