b6141d72418109b95e8a6d361a220eabfcb515f7
imangali <i.kaliakhmetov@strongte.am>
2023-09-17 15:46:10 +0600

    user model

--- application/models/src/main/java/com/zefir/models/User.java
+++ application/models/src/main/java/com/zefir/models/User.java
@@ -1,5 +1,35 @@
 package com.zefir.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
 public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String email;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public enum UserRole{
+        USER,
+        ADMIN;
+    }
 
 }
